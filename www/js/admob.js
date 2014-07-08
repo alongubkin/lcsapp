375c172bf5aad15debb7d993aa72b8024fc6378d
Alon Gubkin <alon.gubkin@gmail.com>
2014-07-08 21:03:51 +0300

    chore(): admob for wp8 and ios

--- www/js/admob.js
+++ www/js/admob.js
@@ -1,8 +1,36 @@
-document.addEventListener("deviceready", function () {
-  var admob_ios_key = '';
-  var admob_android_key = 'ca-app-pub-2708584265734479/7188115543';
-  var adId = (navigator.userAgent.indexOf('Android') >=0) ? admob_android_key : admob_ios_key;
+var isMobile = {
+    Android: function() {
+        return /Android/i.test(navigator.userAgent);
+    },
+    BlackBerry: function() {
+        return /BlackBerry/i.test(navigator.userAgent);
+    },
+    iOS: function() {
+        return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+    },
+    Windows: function() {
+        return /IEMobile/i.test(navigator.userAgent);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+    }
+};
 
+document.addEventListener("deviceready", function () {
+  var admob_ios_key = 'ca-app-pub-2708584265734479/3881395540';
+  var admob_android_key = 'ca-app-pub-2708584265734479/7188115543';
+  var admob_wp8_key = 'ca-app-pub-2708584265734479/8311595140';
+  
+  var adId;
+  
+  if (isMobile.Android()) {
+    adId = admob_android_key;
+  } else if (isMobile.iOS()) {
+    adId = admob_ios_key;
+  } else if (isMobile.Windows()) {
+    adId = admob_wp8_key;
+  }
+  
   if (window.plugins && window.plugins.AdMob) {
     var am = window.plugins.AdMob;
 
