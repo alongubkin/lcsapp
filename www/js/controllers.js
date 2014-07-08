4d400c162b2417d00be0b75b48afd3f8834e5ae7
Alon Gubkin <alon.gubkin@gmail.com>
2014-07-08 03:13:56 +0300

    refactor(admob): seperate to a different file

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -82,28 +82,6 @@ angular.module('lcs.controllers', [])
     $scope.goToMatch = function (match) {
       $state.go('app.match', { matchId: match.matchId });
     };
-    
-    document.addEventListener("deviceready", function () {
-      var admob_ios_key = '';
-      var admob_android_key = 'ca-app-pub-2708584265734479/7188115543';
-      var adId = (navigator.userAgent.indexOf('Android') >=0) ? admob_android_key : admob_ios_key;
-
-      if (window.plugins && window.plugins.AdMob) {
-        var am = window.plugins.AdMob;
-
-        am.createBannerView({
-          'publisherId': adId,
-          'adSize': am.AD_SIZE.BANNER,
-          'bannerAtTop': false,
-          'overlap': false
-        }, function() {
-          am.requestAd(
-            { 'isTesting': false }, 
-            function () { am.showAd(true); }, 
-            function(){ console.log('failed to request ad'); });
-        }, function(){ console.log('failed to create banner view'); });
-      }
-    }, false);
   })
   
   .controller('StandingsCtrl', function ($scope, $localStorage, StandingsService) {
